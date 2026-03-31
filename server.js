9cbbb9691d12f8042232ff0f553c68520da200c6
EPGibbons <EPGibbons@mavs.coloradomesa.edu>
2026-03-31 16:01:42 -0600

    push

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// API routes
+const apiRoutes = require('./routes/api');
+app.use('/api', apiRoutes);
+
+// Page routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/html/index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
